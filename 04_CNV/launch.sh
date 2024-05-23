6539501b9daea6553c18476904af873a8420ccc8
aberbelgara <aberbelgara@alumni.unav.es>
2024-05-23 13:21:03 +0200

    create pipeline1 sbs archive

--- 04_CNV/launch.sh
+++ 04_CNV/launch.sh
@@ -6,7 +6,7 @@ REFERENCE=/scratch/$USER/trabajo-final-bioinformatics/samples/references/GRCh38.
 DICT_REFERENCE=/scratch/$USER/trabajo-final-bioinformatics/samples/references/Homo_sapiens_assembly38.dict
 OUTPUT_DIR=/scratch/$USER/trabajo-final-bioinformatics/04_CNV/Output
 INTERVALS=/scratch/$USER/trabajo-final-bioinformatics/samples/tutorial_11682/targets_C.interval_list
-TUMOR=/scratch/$USER/trabajo-final-bioinformatics/samples/tutorial_11682/tumor.bam
+TUMOR=/scratch/$USER/trabajo-final-bioinformatics/samples/tutorial_11682/tumor.bam #este va a ser el bam que en el sbs lo meta como TUMOR
 GATK=/scratch/$USER/trabajo-final-bioinformatics/bin/gatk-4.3.0.0/gatk
 
 
@@ -14,7 +14,7 @@ reference_fa=/scratch/$USER/trabajo-final-bioinformatics/samples/references/GRCh
 
 # variant calling for each of the bams
 bams_path=/scratch/$USER/trabajo-final-bioinformatics/02_align/dna_bams
-bams=($(ls $bams_path/*.bam))
+bams=($(ls $bams_path/*_recalibrated.bam))
 
 mkdir -p logs
 
