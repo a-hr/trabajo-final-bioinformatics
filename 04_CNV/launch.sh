2b9b5e91c445d4c73c116ecb6d82c35142a8c11e
aberbelgara <aberbelgara@alumni.unav.es>
2024-05-27 20:22:05 +0200

    call pipieline1 in launch.sh

--- 04_CNV/launch.sh
+++ 04_CNV/launch.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 
-
-
-REFERENCE=/scratch/$USER/trabajo-final-bioinformatics/samples/references/GRCh38.primary_assembly.genome.fa
-DICT_REFERENCE=/scratch/$USER/trabajo-final-bioinformatics/samples/references/Homo_sapiens_assembly38.dict
-OUTPUT_DIR=/scratch/$USER/trabajo-final-bioinformatics/04_CNV/Output
-INTERVALS=/scratch/$USER/trabajo-final-bioinformatics/samples/tutorial_11682/targets_C.interval_list
-TUMOR=/scratch/$USER/trabajo-final-bioinformatics/samples/tutorial_11682/tumor.bam #este va a ser el bam que en el sbs lo meta como TUMOR
-GATK=/scratch/$USER/trabajo-final-bioinformatics/bin/gatk-4.3.0.0/gatk
-
-
-reference_fa=/scratch/$USER/trabajo-final-bioinformatics/samples/references/GRCh38.primary_assembly.genome.fa
+cd /scratch/$USER/trabajo-final-bioinformatics/04_CNV
 
 # variant calling for each of the bams
 bams_path=/scratch/$USER/trabajo-final-bioinformatics/02_align/dna_bams
@@ -18,8 +8,9 @@ bams=($(ls $bams_path/*_recalibrated.bam))
 
 mkdir -p logs
 
-# base recalibration
-jID2=$(sbatch --array=0-$(( ${#bams[@]} - 1 )) --dependency=afterok:$jID1 base_recalibrator.sbs $bams_path ./vcf $reference_fa no)
-jID2=$(echo $jID2 | cut -d ' ' -f 4)  # extract number from jID
+# launch fastqc
+jID=$(sbatch --array=0-$((${#bams[@]} - 1))%5 pipeline1.sbs $bams_path)
+# extract number from jID
+jID=$(echo $jID | cut -d ' ' -f 4)
 
 
